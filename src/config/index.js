7655f9ab6e100304bab25b3081a202e77c84148f
fanglikai <1505705886@163.com>
2019-04-19 14:09:20 +0800

    2

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,6 @@
 
-// export const baseUrl = 'http://47.110.155.113:9000/consumer-zhifubao/'; // 线上环境
-export const baseUrl = 'https://www.huizustore.com/consumer-zhifubao/'; // 线上环境
 
-
-// export const baseUrl = 'http://192.168.2.50:9000/consumer-zhifubao/';
-// export const baseUrl = 'http://192.168.103.64:9000/consumer-zhifubao/';
-// export const baseUrl = 'http://192.168.103.215:9000/consumer-zhifubao/';
-// export const baseUrl = 'http://192.168.2.11:9000/consumer-zhifubao/';
+export const baseUrl = 'https'; // 线上环境
 
 // 是否打印信息
 export const noConsole = true;
