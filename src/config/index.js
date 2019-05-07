f4bba4a399d99cd9ba985e149e38209ffdd1e72f
xiaorui <xiaorui@zudeapp.com>
2019-05-07 11:20:40 +0800

    请求地址改变

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 
-// export const baseUrl = 'https://www.huizustore.com/consumer-zhifubao/'; // 线上环境
-export const baseUrl = 'http://192.168.2.2:9000/consumer-zhifubao/'; // 线上环境
+export const baseUrl = 'https://www.huizustore.com/consumer-zhifubao/'; // 线上环境
+// export const baseUrl = 'http://192.168.2.2:9000/consumer-zhifubao/'; // 线上环境
 
 
 // 是否打印信息
